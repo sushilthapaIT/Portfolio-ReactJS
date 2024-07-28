3b64aeea47326521ebe73e0388bc8549e3ec2ce4
Sushil <kidsushilink@gmail.com>
2024-07-27 22:22:22 -0700

    -- commit one
    -- removed unnecessary files
    -- added temporary image for logo
    -- adding content and styling for navbar

---
+++ portfolio-sushi/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Navbar = () => {
+  return (
+    <MainHeader>
+      {/* <img className='logo' src='./images/temp-logo.jpg' alt='logo of sushil portfolio website' /> */}
+      <p>My Logo</p>
+    </MainHeader>
+  )
+}
+
+export default Navbar
+
+const MainHeader = styled.header`
+  
+`
