012d9b2818b8acf166a526ec0baf51af0c42f8f9
Sushil <kidsushilink@gmail.com>
2024-07-28 01:25:30 -0700

    testing if the code is working
    -- this code is not rendering in the browser
    -- testing phase
    -- added react-route-dom for routing

--- portfolio-sushi/src/App.jsx
+++ portfolio-sushi/src/App.jsx
@@ -1,11 +1,30 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom'
+import Home from './content/Home'
+import About from './content/About'
+import Contact from './content/Contact'
 
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: "/home",
+      element: <><Navbar/> <Home/></>
+    },
+    {
+      path: "/about",
+      element: <><Navbar/> <About/></>
+    },
+    {
+      path: "/contact",
+      element: <><Navbar/> <Contact/></>
+    }
+  ])
+
   return (
     <>
-      <Navbar />
+      <RouterProvider router={router}/>
     </>
   )
 }
--- portfolio-sushi/src/components/Navbar.jsx
+++ portfolio-sushi/src/components/Navbar.jsx
@@ -1,28 +1,51 @@
-import React from 'react'
-import styled from 'styled-components'
-import {NavLink, Link} from 'react-router-dom'
+import React from 'react';
+import styled from 'styled-components';
+import { NavLink, Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <MainHeader>
+    <header>
       {/* <img className='logo' src='./images/temp-logo.jpg' alt='logo of sushil portfolio website' /> */}
-      <p>My Logo</p>
-      <NavLinks>
-        <li><NavLink to=''>Home</NavLink></li>
-        <li><NavLink to=''>Projects</NavLink></li>
-        <li><NavLink to=''>About</NavLink></li>
-      </NavLinks>
-      <Link className='cta' href=''><button>Contact</button></Link>
-
-    </MainHeader>
-  )
+      <p className='logo'>My Logo</p>
+      <nav>
+        <li><a to='/'>Home</a></li>
+        <li><a to='/projects'>Projects</a></li>
+        <li><a to='/about'>About</a></li>
+      </nav>
+      <a className='cta' to='/contact'><button>Contact</button></a>
+    </header>
+  );
 }
 
-export default Navbar
+export default Navbar;
 
-const MainHeader = styled.header`
-  
-`
-const NavLinks = styled.nav`
-  
-`
+// const MainHeader = styled.header`
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
+//   padding: 30px 10%;
+
+//   .logo {
+//     cursor: pointer;
+//   }
+// `;
+
+// const NavLinks = styled.ul`
+//   list-style: none;
+//   display: flex;
+//   gap: 20px;
+
+//   li {
+//     margin: 0;
+//   }
+
+//   a {
+//     text-decoration: none;
+//     color: inherit;
+//     font-weight: bold;
+//   }
+
+//   a.active {
+//     color: #007BFF; 
+//   }
+// `;
