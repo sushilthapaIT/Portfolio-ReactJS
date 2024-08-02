3c24143fa8c441c1b3c8b570717377ce098c736b
Sushil <kidsushilink@gmail.com>
2024-08-02 08:11:23 -0700

    added content and styling for the footer component

--- portfolio-sushi/src/components/Footer.jsx
+++ portfolio-sushi/src/components/Footer.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
 
 const Footer = () => {
+    const year = new Date().getFullYear()
   return (
-    <div>
-      <hr />
-    </div>
-  )
+    <FooterBelow>
+        <FooterCopyRight>
+            <p>&#169; {year} Sushil. All right reserved.</p>
+        </FooterCopyRight>
+            <FooterLinks>
+                <a href="" target="_blank"><p>Terms &#38; Conditions</p></a>
+                <a href="" target="_blank"><p>Privacy</p></a>
+                <a href="" target="_blank"><p>Security</p></a>
+                <a href="" target="_blank"><p>Cookie Declaration</p></a>
+            </FooterLinks>
+    </FooterBelow>
+)
 }
 
+
 export default Footer
+
+
+const FooterBelow = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    margin-top: 0%.2rem;
+`
+
+const FooterCopyRight = styled.div`
+    color: white;
+    font-size: 13px;
+    font-weight: 600;
+    line-height: 15px;
+`
+
+const FooterLinks = styled.div`
+    display: flex;
+    flex-direction: row;
+
+    p{
+        font-size: 13px;
+        line-height: 15px;
+        color: white;
+        margin-left: 2rem;
+        font-weight: 600;
+    }
+
+`
